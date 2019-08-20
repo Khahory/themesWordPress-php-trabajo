4e0e54abcf1b028cfe8a8185b6dc05886c836f5e
Khahory <angeld05@live.com>
2019-08-19 22:39:48 -0400

    Widgets agregado

--- index.php
+++ index.php
@@ -26,17 +26,28 @@
     <div id="container" class="group">
         <header class="group">
             <h1>
-                <img src="<?php print IMAGES; ?>/logo.jpg" alt="<?php bloginfo('name'); ?>" />
+                <img src="<?php print IMAGES; ?>/logo1.jpg" alt="<?php bloginfo('name'); ?>" />
             </h1>
             <?php get_search_form(); ?>
+            <?php wp_nav_menu(array('menu' => 'Main', 'container' => 'nav' )); ?>
         </header>
     </div>
+
+    <?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
+        <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+            <?php dynamic_sidebar( 'home_right_1' ); ?>
+        </div><!-- #primary-sidebar -->
+    <?php endif; ?>
 </div>
 <!--Fin del wrapper-->
 
 <!--Inicio del footer-->
 <footer class="group">
+    <p>
+        <?php bloginfo('name'); ?><br> Todos los drechos reservados
+    </p>
 </footer>
+<?php wp_footer();?> <!--De esta manera es que llamamos a los componentes de Wordpress, los declaramos en functions-->
 <!--fin del footer-->
 
 </body>
